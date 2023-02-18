04d7972c4db09d56f6d7e7628a343258292875f3
Bangseungjae <tmdwo5825@naver.com>
2023-02-18 15:28:26 +0900

    init

---
+++ accounts/src/main/java/bsj/io/accounts/Accounts.java
@@ -0,0 +1,27 @@
+package bsj.io.accounts;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@ToString
+public class Accounts {
+
+    @Column(name = "customer_id")
+    private int customerId;
+    @Column(name="account_number")
+    @Id
+    private long accountNumber;
+    @Column(name="account_type")
+    private String accountType;
+    @Column(name = "branch_address")
+    private String branchAddress;
+    @Column(name = "create_dt")
+    private LocalDate createdAt;
+}
---
+++ accounts/src/test/java/bsj/io/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package bsj.io.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
