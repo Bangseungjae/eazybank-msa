a37a96d37f79b9649650502e37bfbce481aee13b
Bangseungjae <tmdwo5825@naver.com>
2023-02-18 18:40:24 +0900

    loans clear

---
+++ loans/src/main/java/bsj/io/loans/controller/LoansController.java
@@ -0,0 +1,48 @@
+package bsj.io.loans.controller;
+
+import bsj.io.loans.config.LoansServiceConfig;
+import bsj.io.loans.model.Customer;
+import bsj.io.loans.model.Loans;
+import bsj.io.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class LoansController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoansController.class);
+    @Autowired
+    private LoansRepository loansRepository;
+
+    @Autowired
+    private LoansServiceConfig loansConfig;
+
+    @PostMapping("/myLoans")
+    public List<Loans> getLoansDetails(@RequestHeader("eazybank-correlation-id")String correlationid, @RequestBody Customer customer) {
+        logger.info("getLoansDetails() method started");
+        List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+        logger.info("getLoansDetails() method ended");
+        if (loans != null) {
+            return loans;
+        } else {
+            return null;
+        }
+
+    }
+
+    @GetMapping("/loans/properties")
+    public String getPropertiesDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+                loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
+    }
+}
---
+++ loans/src/main/java/bsj/io/loans/repository/LoansRepository.java
@@ -0,0 +1,13 @@
+package bsj.io.loans.repository;
+
+import bsj.io.loans.model.Loans;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoansRepository extends CrudRepository<Loans, Long> {
+
+    List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+}
